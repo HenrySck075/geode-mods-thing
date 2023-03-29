96cf22719115035d7217a4f7d18fb0553469b60d
Henry Spheria <111231428+HenrySck075@users.noreply.github.com>
2023-03-29 22:30:29 +0700

    d

---
+++ HashtagSuggestions/player/main.cpp
@@ -0,0 +1,29 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/loader/Log.hpp>
+
+using namespace geode::prelude;
+
+
+class $modify(PlayLayer) {
+	bool init(GJGameLevel*h) {
+		PlayLayer::init(h);
+		//i have no idea
+		auto level = getChildOfType<CCLayer>(this, 0);
+		auto idkhowtousesprbatchnode = CCLayer::create();
+		idkhowtousesprbatchnode->setID("iamdumb");
+		level->addChild(idkhowtousesprbatchnode);
+		return true;
+	}
+	void markCheckpoint() {
+		if (m_fields->m_isDualMode) {
+			auto level = getChildOfType<CCLayer>(this, 0);
+			auto asmentionedabove = level->getChildByID("iamdumb");
+			auto fakecheckpoint = CCSprite::create("checkpoint_01_001.png");
+			fakecheckpoint->setPosition(m_fields->m_player2->getPosition());
+			asmentionedabove->addChild(fakecheckpoint);
+		}
+		geode::log::info(m_fields->m_isDualMode ? "true" : "false");
+		PlayLayer::markCheckpoint();
+	}
+};
