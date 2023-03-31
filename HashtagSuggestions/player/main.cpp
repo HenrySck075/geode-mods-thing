928d0eed3f74de7ba47670e3992202e75eee3934
Henry Spheria <111231428+HenrySck075@users.noreply.github.com>
2023-03-31 21:59:45 +0700

    player - update main.cpp

--- HashtagSuggestions/player/main.cpp
+++ HashtagSuggestions/player/main.cpp
@@ -15,6 +15,9 @@ class $modify(PlayLayer) {
 		level->addChild(idkhowtousesprbatchnode);
 		return true;
 	};
+    void addObject(GameObject* p0) {
+        if (p0->getType() != GameObjectType::Decoration && Mod::get()->getSettingValue<bool>("ultra-ldm")) PlayLayer::addObject(p0);
+    };
 	void markCheckpoint() {
 		if (m_fields->m_isDualMode) {
 			auto level = getChildOfType<CCLayer>(this, 0);
@@ -26,4 +29,4 @@ class $modify(PlayLayer) {
 		geode::log::info(m_fields->m_isDualMode ? "true" : "false");
 		PlayLayer::markCheckpoint();
 	}
-};
+}
