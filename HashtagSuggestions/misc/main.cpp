6b27a77699cb9f27dde20d2b81b529ccd9ef3374
Henry Spheria <111231428+HenrySck075@users.noreply.github.com>
2023-04-07 16:32:21 +0700

    misc - transition flip

--- HashtagSuggestions/misc/main.cpp
+++ HashtagSuggestions/misc/main.cpp
@@ -3,6 +3,10 @@
 #include <Geode/modify/CCTransitionCrossFade.hpp>
 #include <Geode/modify/CCTransitionFadeBL.hpp>
 #include <Geode/modify/CCTransitionFadeTR.hpp>
+#include <Geode/modify/CCTransitionFadeDown.hpp>
+#include <Geode/modify/CCTransitionFadeUp.hpp>
+#include <Geode/modify/CCTransitionFlipAngular.hpp>
+#include <Geode/modify/CCTransitionFlip.hpp>
 #include <Geode/modify/CCTransitionScene.hpp>
 
 float transitionSpeed = (float) Mod::get()->getSettingValue<double>("transition-speed");
@@ -22,10 +26,47 @@ class $modify(CCTransitionFade) {
 };
 
 class $modify(CCTransitionFadeBL) {
-    static CCTransitionFadeBL* create(float duration, CCScene* ) {
+    static CCTransitionFadeBL* create(float duration, CCScene* scene) {
         return CCTransitionFadeBL::create(transitionSpeed, scene);
     }
-}
+};
+
+class $modify(CCTransitionFadeTR) {
+    static CCTransitionFadeTR* create(float duration, CCScene* scene) {
+        return CCTransitionFadeTR::create(transitionSpeed, scene);
+    }
+};
+
+class $modify(CCTransitionFadeDown) {
+    static CCTransitionFadeDown* create(float duration, CCScene* scene) {
+        return CCTransitionFadeDown::create(transitionSpeed, scene);
+    }
+};
+
+class $modify(CCTransitionFadeUp) {
+    static CCTransitionFadeUp* create(float duration, CCScene* scene) {
+        return CCTransitionFadeUp::create(transitionSpeed, scene);
+    }
+};
+
+class $modify(CCTransitionFlipAngular) {
+    static CCTransitionFlipAngular* create(float duration, CCScene* scene, tOrientation o) {
+        return CCTransitionFlipAngular::create(transitionSpeed, scene, o);
+    }
+    static CCTransitionFlipAngular* create(float duration, CCScene* scene) {
+        return CCTransitionFlipAngular::create(transitionSpeed, scene);
+    }
+};
+
+class $modify(CCTransitionFlipX) {
+    static CCTransitionFlipX* create(float duration, CCScene* scene, tOrientation o) {
+        return CCTransitionFlipX::create(transitionSpeed, scene, o);
+    }
+    static CCTransitionFlipX* create(float duration, CCScene* scene) {
+        return CCTransitionFlipX::create(transitionSpeed, scene);
+    }
+};
+
 $execute {
     listenForSettingChanges("transition-speed", +[](double v) {
         float transitionSpeed = (float) v;
