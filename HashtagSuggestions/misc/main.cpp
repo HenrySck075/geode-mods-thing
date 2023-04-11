bfadac8263cc2d83aa01fb55ef9c0005617b6e15
Henry Spheria <111231428+HenrySck075@users.noreply.github.com>
2023-04-11 09:54:15 +0700

    misc - why the heck is m_difficulty a string

--- HashtagSuggestions/misc/main.cpp
+++ HashtagSuggestions/misc/main.cpp
@@ -1,8 +1,11 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCTransitionFade.hpp> //only this is available fsr
-
-using namespace geode::prelude;
+#include <Geode/utils/cocos.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+using namespace geode::prelude;
 #define setTS float transitionSpeed = (float) Mod::get()->getSettingValue<double>("transition-speed");
 class $modify(CCTransitionFade) {
     virtual bool initWithDuration(float d, CCScene* s, ccColor3B const& c) {
@@ -19,6 +22,56 @@ $execute {
 }
 */
 
+class PerformRandomLevel : public CCLayer {
+protected:
+    bool init() {
+        auto winSize=CCDirector::sharedDirector()->getWinSize();
 
-//change transition speed
+        auto bg = CCSprite::create("GJ_gradientBG.png");
+        auto bgSize = bg->getContentSize();
 
+        bg->setScale(winSize.width / bgSize.width, winSize.height / bgSize.height);
+        bg->setAnchorPoint(CCPoint{0,0});
+        bg->setZOrder(-1);
+
+        auto thetext = CCLabelBMFont::create("Getting random level", "bigFont.fnt");
+        thetext->setContentSize(thrtext->getContentSize() * 1.5f);
+        thetext->setPosition(winSize / 2 + CCPoint{0,30});
+
+
+        this->addChild(bg); this->addChild(thetext);
+        LoadingCircle::create()->show();
+        return true;
+    }
+public:
+    static PerformRandomLevel* create() {
+        auto pret = new PerformRandomLevel();
+        if (pret && pret->init()) {
+            pret->autorelease();
+            return pret;
+        };
+        CC_SAFE_DELETE(pret);
+        return nullptr;
+    }
+    void pushLayerWithTransition(float duration, CCLayer* layer) {
+        auto scene = CCScene::create();
+        scene->addChild(layer);
+        CCDirector->sharedDirector()->pushSceneWithTransition(CCTransitionFade::create(duration, scene));
+    }
+    void onBtn(CCObject*h ) {
+        //it might be __pad48 but idk
+        auto searchlay = static_cast<LevelSearchLayer*>(static_cast<CCNode*>(h)->getParent()->getParent());
+        auto searchObj = searchlay->getSearchObject(SearchType::Search, gd::string(searchlay->m_searchInput->getString());
+
+        setTS
+        this->pushLayerWithTransition(transitionSpeed, PerformRandomLevel::create());
+
+        json the;
+        the["secret"] = "Wmfd2893gb7";
+        the["type"] = 0;
+        
+
+        
+
+    }
+}
