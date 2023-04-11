a67f2faa092dd05555e8381ba93ccb8dba7e840a
Henry Spheria <111231428+HenrySck075@users.noreply.github.com>
2023-04-11 08:22:11 +0700

    misc - TODO: use web request to get levels

--- HashtagSuggestions/misc/main.cpp
+++ HashtagSuggestions/misc/main.cpp
@@ -1,77 +1,23 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCTransitionFade.hpp>
-#include <Geode/modify/CCTransitionCrossFade.hpp>
-#include <Geode/modify/CCTransitionFadeBL.hpp>
-#include <Geode/modify/CCTransitionFadeTR.hpp>
-#include <Geode/modify/CCTransitionFadeDown.hpp>
-#include <Geode/modify/CCTransitionFadeUp.hpp>
-#include <Geode/modify/CCTransitionFlipAngular.hpp>
-#include <Geode/modify/CCTransitionFlip.hpp>
-#include <Geode/modify/CCTransitionScene.hpp>
+#include <Geode/modify/CCTransitionFade.hpp> //only this is available fsr
 
-float transitionSpeed = (float) Mod::get()->getSettingValue<double>("transition-speed");
-class $modify(CCTransitionCrossFade) {
-    static CCTransitionCrossFade* create(float duration, CCScene* scene) {
-        return CCTransitionCrossFade::create(transitionSpeed,scene);
-    }
-};
-
+using namespace geode::prelude;
+#include <Geode/modify/LevelSearchLayer.hpp>
+#define setTS float transitionSpeed = (float) Mod::get()->getSettingValue<double>("transition-speed");
 class $modify(CCTransitionFade) {
-    static CCTransitionFade* create(float duration, CCScene* scene) {
-        return CCTransitionFade::create(transitionSpeed, scene);
-    }
-    static CCTransitionFade* create(float duration, CCScene* scene, ccColor3B const& color) {
-        return CCTransitionFade::create(transitionSpeed, scene, color);
+    virtual bool initWithDuration(float d, CCScene* s, ccColor3B const& c) {
+        setTS
+        return CCTransitionFade::initWithDuration(transitionSpeed,s,c);
     }
 };
-
-class $modify(CCTransitionFadeBL) {
-    static CCTransitionFadeBL* create(float duration, CCScene* scene) {
-        return CCTransitionFadeBL::create(transitionSpeed, scene);
-    }
-};
-
-class $modify(CCTransitionFadeTR) {
-    static CCTransitionFadeTR* create(float duration, CCScene* scene) {
-        return CCTransitionFadeTR::create(transitionSpeed, scene);
-    }
-};
-
-class $modify(CCTransitionFadeDown) {
-    static CCTransitionFadeDown* create(float duration, CCScene* scene) {
-        return CCTransitionFadeDown::create(transitionSpeed, scene);
-    }
-};
-
-class $modify(CCTransitionFadeUp) {
-    static CCTransitionFadeUp* create(float duration, CCScene* scene) {
-        return CCTransitionFadeUp::create(transitionSpeed, scene);
-    }
-};
-
-class $modify(CCTransitionFlipAngular) {
-    static CCTransitionFlipAngular* create(float duration, CCScene* scene, tOrientation o) {
-        return CCTransitionFlipAngular::create(transitionSpeed, scene, o);
-    }
-    static CCTransitionFlipAngular* create(float duration, CCScene* scene) {
-        return CCTransitionFlipAngular::create(transitionSpeed, scene);
-    }
-};
-
-class $modify(CCTransitionFlipX) {
-    static CCTransitionFlipX* create(float duration, CCScene* scene, tOrientation o) {
-        return CCTransitionFlipX::create(transitionSpeed, scene, o);
-    }
-    static CCTransitionFlipX* create(float duration, CCScene* scene) {
-        return CCTransitionFlipX::create(transitionSpeed, scene);
-    }
-};
-
+//event listener broke in beta.13
+/*
 $execute {
     listenForSettingChanges("transition-speed", +[](double v) {
         float transitionSpeed = (float) v;
     });
 }
+*/
 
 
 //change transition speed
